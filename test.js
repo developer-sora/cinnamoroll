38a114640916f2be92109a9f4310b3403bc64f0a
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-08-06 13:22:09 +0900

    시나모롤 홈페이지

---
+++ test.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+
+    $('#capuId').mouseover(
+        
+    function() {
+    $("#capuId").css({"background" : "white",
+        "border-radius" : "10%",
+        "border-bottom" : "10px solid skyblue",
+        "opacity" : "1"
+    });
+
+    });
+
+    $('#capuId').mouseover(
+        
+    function() {
+    
+    });
+    
+
+    $("#capuId").mouseleave
+    (function(){
+
+        $("#capuId").css({"background" : "none",
+        "border-radius" : "10%",
+        "border-bottom" : "0px solid pink",
+        "opacity" : "1"
+    });  
+
+});
+$('#moca').mouseover(
+        
+    function() {
+    $("#moca").css({"background" : "white",
+        "border-radius" : "10%",
+        "border-bottom" : "10px solid pink",
+        "opacity" : "1"
+    });
+
+    });
+
+    $('#moca').mouseover(
+        
+    function() {
+    
+    });
+    
+
+    $("#moca").mouseleave
+    (function(){
+
+        $("#moca").css({"background" : "none",
+        "border-radius" : "10%",
+        "border-bottom" : "0px solid pink",
+        "opacity" : "1"
+    });  
+
+});
+$('#she').mouseover(
+        
+    function() {
+    $("#she").css({"background" : "white",
+        "border-radius" : "10%",
+        "border-bottom" : "10px solid skyblue",
+        "opacity" : "1"
+    });
+
+    });
+
+    $('#she').mouseover(
+        
+    function() {
+    
+    });
+    
+
+    $("#she").mouseleave
+    (function(){
+
+        $("#she").css({"background" : "none",
+        "border-radius" : "10%",
+        "border-bottom" : "0px solid pink",
+        "opacity" : "1"
+    });  
+
+});  $('#esspresso').mouseover(
+        
+    function() {
+    $("#esspresso").css({"background" : "white",
+        "border-radius" : "10%",
+        "border-bottom" : "10px solid skyblue",
+        "opacity" : "1"
+    });
+
+    });
+
+    $('#esspresso').mouseover(
+        
+    function() {
+    
+    });
+    
+
+    $("#esspresso").mouseleave
+    (function(){
+
+        $("#esspresso").css({"background" : "none",
+        "border-radius" : "10%",
+        "border-bottom" : "0px solid pink",
+        "opacity" : "1"
+    });  
+
+});  $('#milk').mouseover(
+        
+    function() {
+    $("#milk").css({"background" : "white",
+        "border-radius" : "10%",
+        "border-bottom" : "10px solid pink",
+        "opacity" : "1"
+    });
+
+    });
+
+    $('#milk').mouseover(
+        
+    function() {
+    
+    });
+    
+
+    $("#milk").mouseleave
+    (function(){
+
+        $("#milk").css({"background" : "none",
+        "border-radius" : "10%",
+        "border-bottom" : "0px solid pink",
+        "opacity" : "1"
+    });  
+
+});  });  
